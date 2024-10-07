733721625a5b533cb51579d962fb30f8b747966b
Yuanchen Zhou <yuanchen_zhou@brown.edu>
2024-10-07 15:36:50 -0400

    Distillation code

--- qgtag/simple/pfn/pfn_train_batch.py
+++ qgtag/simple/pfn/pfn_train_batch.py
@@ -3,40 +3,41 @@ import os
 pfn_latent_sizes = [1,2,4,8,16,32,64,128,256,512]
 pfn_phi_sizes = [50,100,250,500]
 
-for l in pfn_latent_sizes:
-    for phi in pfn_phi_sizes:
-        label = "latent"+str(l)+"_phi"+str(phi)+'_pfn'
+for i in range(10):
+    for l in pfn_latent_sizes:
+        for phi in pfn_phi_sizes:
+            label = "latent"+str(l)+"_phi"+str(phi)+'_pfn'
 
-        with open(f'batch/train_{label}.sh', 'w') as run_script:
+            with open(f'batch/train_{label}.sh', 'w') as run_script:
 
-            run_script.write('#!/bin/bash\n\n')
+                run_script.write('#!/bin/bash\n\n')
 
-            run_script.write('#SBATCH -N 1\n')
-            run_script.write('#SBATCH -n 1\n')
-            run_script.write('#SBATCH --mem=96G\n')
-            run_script.write('#SBATCH -t 12:00:00\n')
+                run_script.write('#SBATCH -N 1\n')
+                run_script.write('#SBATCH -n 1\n')
+                run_script.write('#SBATCH --mem=96G\n')
+                run_script.write('#SBATCH -t 12:00:00\n')
 
-            # https://docs.ccv.brown.edu/oscar/gpu-computing/submit-gpu
-            run_script.write('#SBATCH -p gpu --gres=gpu:1\n')
-        
-            run_script.write('#SBATCH -o logs/slurm-%j.out\n')
-            run_script.write('#SBATCH -J PFN_'+label+'\n')
-            run_script.write('\n')
+                # https://docs.ccv.brown.edu/oscar/gpu-computing/submit-gpu
+                run_script.write('#SBATCH -p gpu --gres=gpu:1\n')
 
-            run_script.write('source /users/yzhou276/work/qgtag/qg/bin/activate\n\n')
-            
-            cmd =  'python3 pfn_train.py '
-            cmd += '-nEpochs=200 '
-            cmd += '-batchSize=500 '
-            cmd += '-latentSize='+str(l)+' '
-            cmd += '-phiSizes='+str(phi)+' '
-            cmd += '-doEarlyStopping '
-            cmd += '-patience=10 '
-            cmd += '-usePIDs '
-            cmd += '-ModelNum=0 '
+                run_script.write('#SBATCH -o logs/slurm-%j.out\n')
+                run_script.write('#SBATCH -J PFN_'+label+'\n')
+                run_script.write('\n')
 
-            print(cmd)            
-            run_script.write(cmd+'\n')
-            
-        print('sbatch batch/train_'+label+'.sh')
-        os.system(f'sbatch batch/train_{label}.sh')
+                run_script.write('source /users/yzhou276/work/qgtag/qg/bin/activate\n\n')
+
+                cmd =  'python3 pfn_train.py '
+                cmd += '-nEpochs=200 '
+                cmd += '-batchSize=500 '
+                cmd += '-latentSize='+str(l)+' '
+                cmd += '-phiSizes='+str(phi)+' '
+                cmd += '-doEarlyStopping '
+                cmd += '-patience=10 '
+                cmd += '-usePIDs '
+                cmd += '-ModelNum='+str(i)+' '
+
+                print(cmd)
+                run_script.write(cmd+'\n')
+
+            print('sbatch batch/train_'+label+'.sh')
+            os.system(f'sbatch batch/train_{label}.sh')
--- toptag/student/dnn/dnn_distill_batch.py
+++ toptag/student/dnn/dnn_distill_batch.py
@@ -5,49 +5,50 @@ pfn_latent_sizes = [128]
 #pfn_phi_sizes = [50,100,250,500]
 pfn_phi_sizes = [250]
 
-nlayers = [2] #[2,3,4,5,10]
-dense_sizes = [100] #[1,10,25,50,100,200,300,500]
+nlayers = [5] #[2,3,4,5,10]
+dense_sizes = [25] #[1,10,25,50,100,200,300,500]
 
 alpha_value = 0.0
 
-for l in pfn_latent_sizes:
-    for phi in pfn_phi_sizes:
-        for n in nlayers:
-            for d in dense_sizes:
-                label = f'{n}layers_{d}dense_DNN_by_latent{l}_phi{phi}_pfn'
-                with open(f'batch/train_{label}.sh', 'w') as run_script:
+for i in range(10):
+    for l in pfn_latent_sizes:
+        for phi in pfn_phi_sizes:
+            for n in nlayers:
+                for d in dense_sizes:
+                    label = f'{n}layers_{d}dense_DNN_by_latent{l}_phi{phi}_pfn'
+                    with open(f'batch/train_{label}.sh', 'w') as run_script:
 
-                    run_script.write('#!/bin/bash\n\n')
+                        run_script.write('#!/bin/bash\n\n')
 
-                    run_script.write('#SBATCH -N 1\n')
-                    run_script.write('#SBATCH -n 1\n')
-                    run_script.write('#SBATCH --mem=96G\n')
-                    run_script.write('#SBATCH -t 12:00:00\n')
+                        run_script.write('#SBATCH -N 1\n')
+                        run_script.write('#SBATCH -n 1\n')
+                        run_script.write('#SBATCH --mem=96G\n')
+                        run_script.write('#SBATCH -t 12:00:00\n')
 
-                    # https://docs.ccv.brown.edu/oscar/gpu-computing/submit-gpu
-                    run_script.write('#SBATCH -p gpu --gres=gpu:1\n')
+                        # https://docs.ccv.brown.edu/oscar/gpu-computing/submit-gpu
+                        run_script.write('#SBATCH -p gpu --gres=gpu:1\n')
 
-                    run_script.write('#SBATCH -o logs/slurm-%j.out\n')
-                    run_script.write('#SBATCH -J DNN_'+label+'\n')
-                    run_script.write('\n')
+                        run_script.write('#SBATCH -o logs/slurm-%j.out\n')
+                        run_script.write('#SBATCH -J DNN_'+label+'\n')
+                        run_script.write('\n')
 
-                    run_script.write('source /users/yzhou276/work/toptag/top/bin/activate\n\n')
+                        run_script.write('source /users/yzhou276/work/toptag/top/bin/activate\n\n')
 
-                    cmd =  'python3 dnn_distill.py '
-                    cmd += '-nEpochs=200 '
-                    cmd += '-batchSize=500 '
-                    cmd += '-latentSize='+str(l)+' '
-                    cmd += '-phiSizes='+str(phi)+' '
-                    cmd += '-doEarlyStopping '
-                    cmd += '-patience=10 '
-                    cmd += '-usePIDs '
-                    cmd += '-nLayers='+str(n)+' '
-                    cmd += '-layerSize='+str(d)+' '
-                    cmd += '-alpha='+str(alpha_value)+' '
-                    cmd += '-ModelNum=0 '
+                        cmd =  'python3 dnn_distill.py '
+                        cmd += '-nEpochs=200 '
+                        cmd += '-batchSize=500 '
+                        cmd += '-latentSize='+str(l)+' '
+                        cmd += '-phiSizes='+str(phi)+' '
+                        cmd += '-doEarlyStopping '
+                        cmd += '-patience=10 '
+                        cmd += '-usePIDs '
+                        cmd += '-nLayers='+str(n)+' '
+                        cmd += '-layerSize='+str(d)+' '
+                        cmd += '-alpha='+str(alpha_value)+' '
+                        cmd += '-ModelNum='+str(i)+' '
 
-                    print(cmd)
-                    run_script.write(cmd+'\n')
+                        print(cmd)
+                        run_script.write(cmd+'\n')
 
-                print('sbatch batch/train_'+label+'.sh')
-                os.system(f'sbatch batch/train_{label}.sh')
+                    print('sbatch batch/train_'+label+'.sh')
+                    os.system(f'sbatch batch/train_{label}.sh')
